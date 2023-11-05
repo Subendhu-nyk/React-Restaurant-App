2f2dac5d0e8e4a3d00afc3c5586971f84b823c43
Subendhu-nyk <subendhu.nyk@gmail.com>
2023-11-05 13:50:07 +0530

    Cart without functionality (t-5)

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import Header from './Components/Layout/Header';
 import './App.css';
 import { Fragment } from 'react';
 import Meals from "./Components/Meals/Meals"
+import Cart from './Components/Cart/Cart';
 function App() {
   return (
    <Fragment>
+    <Cart/>
     <Header/>
     <main>
     <Meals/>
